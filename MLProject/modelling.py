b9615138fe4f708654802bafa70765204ebd8e6a
Syarif <syarif.nur92@gmail.com>
2025-09-07 22:23:50 +0700

    update modelling

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,37 +2,47 @@ import mlflow
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-import random
+from sklearn.metrics import precision_score, recall_score, f1_score
 import numpy as np
+import os
+import warnings
+import sys
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
 
-# Create a new MLflow Experiment
-mlflow.set_experiment("Submission")
+    file_path = sys.argv[3] if len(sys.argv) > 3 else os.path.join(os.path.dirname(os.path.abspath(__file__)), "wine-qt_preprocessing.csv")
+    data = pd.read_csv(file_path)
 
-data = pd.read_csv("wine-qt_preprocessing.csv")
-
-X_train, X_test, y_train, y_test = train_test_split(
-    data.drop("label", axis=1),
-    data["label"],
-    random_state=42,
-    test_size=0.2
-)
-input_example = X_train.iloc[0:5]
-
-with mlflow.start_run():
-    # Log parameters
-    n_estimators = 50
-    max_depth = 5
-    mlflow.autolog()
-    # Train model
-    model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="model",
-        input_example=input_example
+    X_train, X_test, y_train, y_test = train_test_split(
+        data.drop("label", axis=1),
+        data["label"],
+        random_state=42,
+        test_size=0.2
     )
-    model.fit(X_train, y_train)
-    # Log metrics
-    accuracy = model.score(X_test, y_test)
-    mlflow.log_metric("accuracy", accuracy)
+    input_example = X_train.iloc[0:5]
+    n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 50
+    max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 5
+
+    with mlflow.start_run():
+        model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
+        model.fit(X_train, y_train)
+
+        predicted = model.predict(X_test)
+
+        mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path="model",
+            input_example=input_example
+        )
+
+        accuracy = model.score(X_test, y_test)
+        precision = precision_score(y_test, predicted)
+        recall = recall_score(y_test, predicted)
+        f1 = f1_score(y_test, predicted)
+
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.log_metric("precision", precision)
+        mlflow.log_metric("recall", recall)
+        mlflow.log_metric("f1_score", f1)
